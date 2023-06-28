7653c36650f145fe5a3aa4dbb72cb993565a0bd9
hangtr124 <124642321+hangtr124@users.noreply.github.com>
2023-06-27 23:02:40 -0500

    Update new home page
    New homepage using Tailwind

---
+++ src/data/dummy.js
@@ -0,0 +1,250 @@
+import React from 'react';
+import { FiShoppingBag, FiEdit, FiPieChart, FiBarChart, FiCreditCard, FiStar, FiShoppingCart } from 'react-icons/fi';
+import {BsCurrencyDollar, BsShield, BsChatLeft } from 'react-icons/bs';
+import { IoMdContacts, IoIosSearch } from 'react-icons/io';
+import { WiAlien } from "react-icons/wi";
+
+import ironman from './ironman.jpg';
+import nick from './nick.jpg';
+import thanos from './thanos.jpg';
+
+export const gridOrderImage = (props) => (
+  <div>
+    <img
+      className="rounded-xl h-20 md:ml-3"
+      src={props.ProductImage}
+      alt="order-item"
+    />
+  </div>
+);
+
+export const gridOrderStatus = (props) => (
+  <button
+    type="button"
+    style={{ background: props.StatusBg }}
+    className="text-white py-1 px-2 capitalize rounded-2xl text-md"
+  >
+    {props.Status}
+  </button>
+);
+
+
+export const links = [
+  {
+    title: 'Dashboard',
+    links: [
+      {
+        name: 'home',
+        icon: <WiAlien />,
+      },
+    ],
+  },
+
+  {
+    title: 'Pages',
+    links: [
+      {
+        name: 'Setting',
+        icon: <IoMdContacts/>,
+      },
+      {
+        name: 'Search',
+        icon: <IoIosSearch/>,
+      },
+      
+    ],
+  },
+  
+];
+
+
+export const chatData = [
+  {
+    image:
+      nick,
+    message: 'Welcome to the Team!',
+    desc: 'Cardio weekend !',
+    time: '9:08 AM',
+  },
+  {
+    image:
+      thanos,
+    message: 'New message received',
+    desc: 'Thanos invited you to his group',
+    time: '11:56 AM',
+  }
+];
+
+
+export const themeColors = [
+  {
+    name: 'blue-theme',
+    color: '#1A97F5',
+  },
+  {
+    name: 'green-theme',
+    color: '#03C9D7',
+  },
+  {
+    name: 'purple-theme',
+    color: '#7352FF',
+  },
+  {
+    name: 'red-theme',
+    color: '#FF5C8E',
+  },
+  {
+    name: 'indigo-theme',
+    color: '#1E4DB7',
+  },
+  {
+    color: '#FB9678',
+    name: 'orange-theme',
+  },
+];
+
+export const userProfileData = [
+  {
+    icon: <BsCurrencyDollar />,
+    title: 'My Profile',
+    desc: 'Account Settings',
+    iconColor: '#03C9D7',
+    iconBg: '#E5FAFB',
+  },
+  {
+    icon: <BsShield />,
+    title: 'My Inbox',
+    desc: 'Messages & Emails',
+    iconColor: 'rgb(0, 194, 146)',
+    iconBg: 'rgb(235, 250, 242)',
+  },
+  {
+    icon: <FiCreditCard />,
+    title: 'My Tasks',
+    desc: 'To-do and Daily Tasks',
+    iconColor: 'rgb(255, 244, 229)',
+    iconBg: 'rgb(254, 201, 15)',
+  },
+];
+
+
+
+export const lineChartData = [
+  [
+    { x: new Date(2005, 0, 1), y: 21 },
+    { x: new Date(2006, 0, 1), y: 24 },
+    { x: new Date(2007, 0, 1), y: 36 },
+    { x: new Date(2008, 0, 1), y: 38 },
+    { x: new Date(2009, 0, 1), y: 54 },
+    { x: new Date(2010, 0, 1), y: 57 },
+    { x: new Date(2011, 0, 1), y: 70 },
+  ],
+  [
+    { x: new Date(2005, 0, 1), y: 28 },
+    { x: new Date(2006, 0, 1), y: 44 },
+    { x: new Date(2007, 0, 1), y: 48 },
+    { x: new Date(2008, 0, 1), y: 50 },
+    { x: new Date(2009, 0, 1), y: 66 },
+    { x: new Date(2010, 0, 1), y: 78 },
+    { x: new Date(2011, 0, 1), y: 84 },
+  ],
+
+  [
+    { x: new Date(2005, 0, 1), y: 10 },
+    { x: new Date(2006, 0, 1), y: 20 },
+    { x: new Date(2007, 0, 1), y: 30 },
+    { x: new Date(2008, 0, 1), y: 39 },
+    { x: new Date(2009, 0, 1), y: 50 },
+    { x: new Date(2010, 0, 1), y: 70 },
+    { x: new Date(2011, 0, 1), y: 100 },
+  ],
+];
+export const dropdownData = [
+  {
+    Id: '1',
+    Time: 'March 2021',
+  },
+  {
+    Id: '2',
+    Time: 'April 2021',
+  }, {
+    Id: '3',
+    Time: 'May 2021',
+  },
+];
+
+
+
+export const pieChartData = [
+  { x: 'Labour', y: 18, text: '18%' },
+  { x: 'Legal', y: 8, text: '8%' },
+  { x: 'Production', y: 15, text: '15%' },
+  { x: 'License', y: 11, text: '11%' },
+  { x: 'Facilities', y: 18, text: '18%' },
+  { x: 'Taxes', y: 14, text: '14%' },
+  { x: 'Insurance', y: 16, text: '16%' },
+];
+
+export const contextMenuItems = [
+  'AutoFit',
+  'AutoFitAll',
+  'SortAscending',
+  'SortDescending',
+  'Copy',
+  'Edit',
+  'Delete',
+  'Save',
+  'Cancel',
+  'PdfExport',
+  'ExcelExport',
+  'CsvExport',
+  'FirstPage',
+  'PrevPage',
+  'LastPage',
+  'NextPage',
+];
+
+export const ecomPieChartData = [
+  { x: '2018', y: 18, text: '35%' },
+  { x: '2019', y: 18, text: '15%' },
+  { x: '2020', y: 18, text: '25%' },
+  { x: '2021', y: 18, text: '25%' },
+];
+
+export const CaloriesBurntPie = [
+  { x: 'Running', y: 450, text: '25%' },
+  { x: 'Lifting', y: 450, text: '25%' },
+  { x: 'Carido', y: 540, text: '30%' },
+  { x: 'Incomplete', y: 360, text: '20%' },
+];
+
+export const CaloriesIntakePie = [
+  { x: 'Breakfast', y: 300, text: '25%' },
+  { x: 'Lunch', y: 450, text: '38%' },
+  { x: 'Snack', y: 250, text: '20%' },
+  { x: 'Incomplete', y: 200, text: '17%' },
+];
+
+export const stackedChartData = [
+  [
+    { x: 'Jan', y: 111.1 },
+    { x: 'Feb', y: 127.3 },
+    { x: 'Mar', y: 143.4 },
+    { x: 'Apr', y: 159.9 },
+    { x: 'May', y: 159.9 },
+    { x: 'Jun', y: 159.9 },
+    { x: 'July', y: 159.9 },
+  ],
+  [
+    { x: 'Jan', y: 111.1 },
+    { x: 'Feb', y: 127.3 },
+    { x: 'Mar', y: 143.4 },
+    { x: 'Apr', y: 159.9 },
+    { x: 'May', y: 159.9 },
+    { x: 'Jun', y: 159.9 },
+    { x: 'July', y: 159.9 },
+  ],
+];
+
+
+
