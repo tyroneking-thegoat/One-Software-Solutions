1c6d877de1d35b5add7f7b2931f6577e481f6471
tyroneking-thegoat <63822174+tyroneking-thegoat@users.noreply.github.com>
2023-07-13 13:35:24 +0000

    Pending changes exported from your codespace

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
 const Search = () => {
   return (
-    <div>
-      <h1>Search</h1>
-    </div>
-  )
-}
+    <Box
+      sx={{
+        width: 800,
+        maxWidth: '100%',
+      }}
+      justifyContent="center"
+    >
+      <TextField fullWidth label="fullwidth" id="fullWidth" />
+
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Header 1</TableCell>
+              <TableCell>Header 2</TableCell>
+              <TableCell>Header 3</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>Cell 1</TableCell>
+              <TableCell>Cell 2</TableCell>
+              <TableCell>Cell 3</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+};
 
 export default Search;
