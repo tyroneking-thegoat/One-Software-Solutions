6736111bf48d299ca8b90c07443c4a5149d36e8c
DominicArgento <136940992+DominicArgento@users.noreply.github.com>
2023-07-16 17:44:02 -0500

    Update App.js
    Added login and create accounts pages along with correct routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
-//import { Pie } from './pages';
 import './App.css';
 
 import { useStateContext } from './contexts/ContextProvider';
+import Login from './pages/Login';
 import Home from './pages/Home';
 import Search from './pages/search';
 import Setting from './pages/setting';
+import CreateAccount from './pages/CreateAccount';
 
 const App = () => {
-  const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
+  const {
+    setCurrentColor,
+    setCurrentMode,
+    currentMode,
+    activeMenu,
+    currentColor,
+    themeSettings,
+    setThemeSettings,
+  } = useStateContext();
 
   useEffect(() => {
     const currentThemeColor = localStorage.getItem('colorMode');
@@ -22,30 +30,74 @@ const App = () => {
       setCurrentColor(currentThemeColor);
       setCurrentMode(currentThemeMode);
     }
+  }, [setCurrentColor, setCurrentMode]);
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const checkAuthentication = () => {
+    // Check if the user is authenticated
+    fetch('http://localhost:3001/login', {
+      method: 'GET',
+      credentials: 'include', // Include cookies in the request
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.loggedIn) {
+          setIsAuthenticated(true);
+          setCurrentUser(data.user);
+        } else {
+          setIsAuthenticated(false);
+          setCurrentUser(null);
+        }
+      })
+      .catch((error) => {
+        console.error('Error checking authentication:', error);
+      });
+  };
+
+  useEffect(() => {
+    checkAuthentication(); // Check authentication when the component mounts
   }, []);
+
+  const handleLogin = () => {
+    setIsAuthenticated(true);
+    checkAuthentication(); // Check authentication after successful login
+  };
+
+  const handleLogout = () => {
+    fetch('http://localhost:3001/logout', {
+      method: 'GET',
+      credentials: 'include', // Include cookies in the request
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data.loggedIn) {
+          setIsAuthenticated(false);
+          setCurrentUser(null);
+        }
+      })
+      .catch((error) => {
+        console.error('Error logging out:', error);
+      });
+  };
 
   return (
     <div className={currentMode === 'Dark' ? 'dark' : ''}>
-      <BrowserRouter>
+      <Router>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
-            <TooltipComponent
-              content="Settings"
-              position="Top"
+            <button
+              type="button"
+              onClick={() => setThemeSettings(true)}
+              style={{ background: currentColor, borderRadius: '50%' }}
+              className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
             >
-              <button
-                type="button"
-                onClick={() => setThemeSettings(true)}
-                style={{ background: currentColor, borderRadius: '50%' }}
-                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
-              >
-                <FiSettings />
-              </button>
-
-            </TooltipComponent>
+              <FiSettings />
+            </button>
           </div>
           {activeMenu ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
               <Sidebar />
             </div>
           ) : (
@@ -56,33 +108,37 @@ const App = () => {
           <div
             className={
               activeMenu
-                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
-                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+                ? 'dark:bg-main-dark-bg bg-main-bg min-h-screen md:ml-72 w-full'
+                : 'bg-main-bg dark:bg-main-dark-bg w-full min-h-screen flex-2'
             }
           >
-            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
-              <Navbar />
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              <Navbar onLogout={handleLogout} />
             </div>
             <div>
-              {themeSettings && (<ThemeSettings />)}
-
+              {themeSettings && <ThemeSettings />}
               <Routes>
-                {/* dashboard  */}
-                <Route path="/" element={(<Home />)} />
-                <Route path="/home" element={(<Home />)} />
-
-                {/* pages  */}
-                <Route path="/search" element={<Search/>} />
-                <Route path="/setting" element={<Setting/>} />
-                
-                
-
+                <Route path="/login" element={<Login onLogin={handleLogin} />} />
+                <Route path="/create-account" element={<CreateAccount />} />
+                <Route
+                  path="/home"
+                  element={isAuthenticated ? <Home currentUser={currentUser} /> : <Navigate to="/login" replace />}
+                />
+                <Route
+                  path="/search"
+                  element={isAuthenticated ? <Search currentUser={currentUser} /> : <Navigate to="/login" replace />}
+                />
+                <Route
+                  path="/setting"
+                  element={isAuthenticated ? <Setting currentUser={currentUser} /> : <Navigate to="/login" replace />}
+                />
+                <Route path="/" element={<Navigate to="/login" replace />} />
               </Routes>
             </div>
             <Footer />
           </div>
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
