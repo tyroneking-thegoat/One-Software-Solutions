da06a8c09d5ea2c673f96c8048ecb76c2a40f3f1
Trevonne <63822174+tyroneking-thegoat@users.noreply.github.com>
2023-07-17 20:26:41 +0000

    Update 4 files

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import papa from "papaparse";
+import data from "./nutrients_csvfil.csv";
 
-const Search = () => {
+const columns = [
+  { field: "Food", headerName: "Food", width: 130 },
+  { field: "Measure", headerName: "Measure", width: 100 },
+  { field: "Grams", headerName: "Grams", width: 100 },
+  { field: "Calories", headerName: "Calories", width: 100 },
+  { field: "Protein", headerName: "Protein", width: 100 },
+  { field: "Fat", headerName: "Fat", width: 100 },
+  { field: "Sat.Fat", headerName: "Sat.Fat", width: 100 },
+  { field: "Fiber", headerName: "Fiber", width: 100 },
+  { field: "Carbs", headerName: "Carbs", width: 100 },
+  { field: "Category", headerName: "Category", width: 100 }
+];
+
+export default function DataTable() {
+  const [data1, setData] = useState([]);
+
+  papa.parse(data, {
+    dynamicTyping: true,
+    header: true,
+    download: true,
+    skipEmptyLines: true,
+    complete: function (result) {
+      setData(result.data);
+    }
+  });
+
   return (
-    <div>
-      <h1>Search</h1>
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={data1}
+        columns={columns}
+        getRowId={(data1) => data1.Grams}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 }
+          }
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
     </div>
-  )
+  );
 }
-
-export default Search;
