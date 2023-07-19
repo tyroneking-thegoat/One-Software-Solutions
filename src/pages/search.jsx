ffae7f4fcb90893b4db674ac75410e99ffaa8063
tyroneking-thegoat <63822174+tyroneking-thegoat@users.noreply.github.com>
2023-07-19 13:39:11 -0500

    Update search.jsx
    more testing

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,27 +1,27 @@
 import React, { useState, useEffect } from "react";
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 import { DataGrid } from "@mui/x-data-grid";
+import Button from "@mui/material/Button";
 import papa from "papaparse";
-import data from "./nutrients_csvfil.csv";
+import data from "./food1.csv";
 
 const columns = [
-  { field: "Food", headerName: "Food", width: 130 },
-  { field: "Measure", headerName: "Measure", width: 100 },
-  { field: "Grams", headerName: "Grams", width: 100 },
-  { field: "Calories", headerName: "Calories", width: 100 },
-  { field: "Protein", headerName: "Protein", width: 100 },
-  { field: "Fat", headerName: "Fat", width: 100 },
-  { field: "Sat.Fat", headerName: "Sat.Fat", width: 100 },
-  { field: "Fiber", headerName: "Fiber", width: 100 },
-  { field: "Carbs", headerName: "Carbs", width: 100 },
-  { field: "Category", headerName: "Category", width: 100 }
+  { field: "ID", headerName: "ID", width: 250 },
+  { field: "Food", headerName: "Food", width: 250 },
+  { field: "Measure", headerName: "Measure", width: 250 },
+  { field: "Calories", headerName: "Calories", width: 250 },
+  { field: "Protien", headerName: "Protein", width: 250 },
+  { field: "Fat", headerName: "Fat", width: 250 },
+  { field: "Carbs", headerName: "Carbs", width: 250 }
 ];
 
 export default function DataTable() {
   const [data1, setData] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredData, setFilteredData] = useState([]);
+  const [select, setselect] = useState([]);
+  const [cal, setcal] = useState();
 
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
@@ -48,12 +48,22 @@ export default function DataTable() {
     });
     setFilteredData(filtered);
   }, [data1, searchQuery]);
-  
+
+  const handleRowClick = (params) => {
+    setselect(params.row.Calories);
+    setcal(params.row.Calories);
+  };
+
+  function handlebuttonclick(select) {
+    setcal(select);
+    console.log(cal);
+  }
+
   return (
     <Box
       sx={{
-        width: 800,
-        maxWidth: '100%',
+        width: "100%",
+        maxWidth: "250%"
       }}
       justifyContent="center"
     >
@@ -62,21 +72,30 @@ export default function DataTable() {
         label="Search"
         id="search"
         value={searchQuery}
-        onChange={handleSearchChange}/>
-    <div style={{ height: 400, width: "100%" }}>
-      <DataGrid
-        rows={filteredData}
-        columns={columns}
-        getRowId={(data1) => data1.Grams}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 }
-          }
-        }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
+        onChange={handleSearchChange}
       />
-    </div>
+      <div style={{ height: "auto", width: "100%" }}>
+        <DataGrid
+          rows={filteredData}
+          columns={columns}
+          getRowId={(data1) => data1.ID}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 10 }
+            }
+          }}
+          pageSizeOptions={[5, 10, 25, 50, 100]}
+          checkboxSelection
+          onRowClick={handleRowClick}
+        />
+        <Button
+          variant="contained"
+          size="large"
+          onClick={() => handlebuttonclick(select)}
+        >
+          Submit
+        </Button>
+      </div>
     </Box>
   );
 }
